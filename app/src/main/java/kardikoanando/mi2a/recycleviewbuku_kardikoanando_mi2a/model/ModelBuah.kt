e731483dfbb5f718148054269c475556e37e3da5
TRKA234 <kardikoanando234@gmail.com>
2024-09-16 00:35:56 +0700

    Update

---
+++ app/src/main/java/kardikoanando/mi2a/recycleviewbuku_kardikoanando_mi2a/model/ModelBuah.kt
@@ -0,0 +1,52 @@
+package kardikoanando.mi2a.recycleviewbuku_kardikoanando_mi2a.model
+
+import kardikoanando.mi2a.recycleviewbuku_kardikoanando_mi2a.R
+
+class ModelBuah (
+    val image : Int,
+    val nama : String
+)
+
+//kita mock data untuk array
+object Mocklist{
+    fun getModel() : List<ModelBuah> {
+        val itemModel1 = ModelBuah(
+            R.drawable.apple,
+            "Apple"
+        )
+        val itemModel2 = ModelBuah(
+            R.drawable.grapes,
+            "Anggur"
+        )
+
+        val itemModel3 = ModelBuah(
+            R.drawable.orange,
+            "Jeruk"
+        )
+
+        val itemModel4 = ModelBuah(
+            R.drawable.pear,
+            "Pir"
+        )
+
+        val itemModel5 = ModelBuah(
+            R.drawable.strawberry,
+            "Stroberi"
+        )
+
+        val itemModel6 = ModelBuah(
+            R.drawable.orange,
+            "Orange"
+        )
+
+        val itemList: ArrayList<ModelBuah> = ArrayList()
+        itemList.add(itemModel1)
+        itemList.add(itemModel2)
+        itemList.add(itemModel3)
+        itemList.add(itemModel4)
+        itemList.add(itemModel5)
+        itemList.add(itemModel6)
+
+        return itemList
+    }
+}
---
+++ app/src/main/java/kardikoanando/mi2a/recycleviewbuku_kardikoanando_mi2a/model/ModelBuku.kt
@@ -0,0 +1,7 @@
+package kardikoanando.mi2a.recycleviewbuku_kardikoanando_mi2a.model
+
+class ModelBuku (
+    val judul : String,
+    val penerbit : String
+
+)
